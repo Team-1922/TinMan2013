a9665288122218474e23e3a2bc6e7741389031eb
Tyler Slabinski <tslabinski@mygsc.com>
2013-02-02 19:30:05 -0500

    Added simple CPP file for Thrower Object.

---
+++ Thrower.cpp
@@ -0,0 +1,24 @@
+#include "Thrower.h"
+
+
+//Constructor
+Thrower::Thrower()
+{	
+}
+
+//Deconstructor
+Thrower::~Thrower()
+{
+}
+
+void Thrower::setRPM(int speed)
+{
+}
+
+void Thrower::setSpeed(float speed)
+{
+}
+
+bool Thrower::isHealthy()
+{
+}
