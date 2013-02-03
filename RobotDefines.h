56086938cd9d95d588e9010f0c1002b81e72484f
stcote <stcote@gmail.com>
2013-02-02 20:17:27 -0500

    Robot Defines

--- RobotDefines.h
+++ RobotDefines.h
@@ -1,44 +1,53 @@
+//******************************************************************************
+//
+// File: RobotDefines.h
+//
+// Description: Defines for all inputs and motors
+//
+//******************************************************************************
 #ifndef RobotDefines_h
 #define RobotDefines_h
 
 // PWMs
 #define PWM_LEFT_DRIVE_MOTOR       (1)
 #define PWM_RIGHT_DRIVE_MOTOR      (2)
-#define PWM_RAMP_MOTOR             (3)
-#define PWM_LOWER_CONVEYOR_MOTOR   (5)
-#define PWM_UPPER_CONVEYOR_MOTOR   (7)
-#define PWM_LAUNCHER_MOTOR         (8)
+#define PWM_THROWER_MOTOR          (3)
+#define PWM_FEEDER_MOTOR           (4)
+#define PWM_ELEVATOR_MOTOR         (5)
+#define PWM_CLIMBER_MOTOR          (6)
 
 
 // Relays
-#define RELAY_SHOOTER_CAM_MOTOR    (1)
-#define RELAY_BALL_COLLECTOR_MOTOR (2)
+#define RELAY_THROWER_MOTOR        (1)
 
 
 // Digital IO
 #define DIG_MOD1                   (1)
 
-#define DIG_CH_IN_SHOOTER_READY    (5)
-#define DIG_CH_IN_RAMP_UP          (6)
-#define DIG_CH_IN_SHOOTER_RPM      (7)
+#define DIG_CH_IN_SHOOTER_RPM      (1)
+#define DIG_CH_IN_FEEDER_SENSOR    (2)
+#define DIG_CH_IN_ELEV_UP_LIMIT    (3)
+#define DIG_CH_IN_ELEV_DN_LIMIT    (4)
+#define DIG_CH_IN_CLIMBER_LIMIT    (5)
 
 
 // Joysticks
 #define PORT_DRIVER_JOYSTICK       (1)
-#define PORT_BALL_COLLECT_JOYSTICK (2)
+#define PORT_THROWER_JOYSTICK      (2)
 
 
 // Joystick Buttons
 
 // Joystick 1
-#define BTN_RAMP_DOWN              (6)
-#define BTN_RAMP_UP                (7)
-#define BTN_READ_CONFIG            (8)
+#define BTN_J1_SHOOT               (1)
+#define BTN_J1_CLIMBER             (4)
 
 // Joystick 2
-#define BTN_SHOOT_BALL             (1)
-#define BTN_TOGGLE_BALL_MOVE       (4)
-#define BTN_REVERSE_BALL_MOVE      (6)
+#define BTN_J2_SPEED_1             (4)
+#define BTN_J2_SPEED_2             (5)
+#define BTN_J2_SPEED_3             (6)
+
+#define THROTTLE_J2_SPEED          (1)
 
 
 #endif
